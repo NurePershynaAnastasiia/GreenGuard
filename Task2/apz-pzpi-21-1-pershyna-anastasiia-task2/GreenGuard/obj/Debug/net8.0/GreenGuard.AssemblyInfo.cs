4594583aaf3c9707747ef6342d29cdef6539b19a
NurePershynaAnastasiia <anastasiia.pershyna@nure.ua>
2024-05-06 21:45:08 +0300

    Deleted ignored files

--- Task2/apz-pzpi-21-1-pershyna-anastasiia-task2/GreenGuard/obj/Debug/net8.0/GreenGuard.AssemblyInfo.cs
+++ Task2/apz-pzpi-21-1-pershyna-anastasiia-task2/GreenGuard/obj/Debug/net8.0/GreenGuard.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GreenGuard")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d5fcb95f65cf45a0bda9ac85a00e77ea0775cdcf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+de5817a9a2c2345d4ae2d83251a90c887033b526")]
 [assembly: System.Reflection.AssemblyProductAttribute("GreenGuard")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GreenGuard")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
